32e932f226353d512f82d2960fc316b4ab5548b7
imanuelg-GitHub <85810404+imanuelg-GitHub@users.noreply.github.com>
2022-07-30 23:49:31 +0100

    Add files via upload

---
+++ Simple.java
@@ -0,0 +1,12 @@
+class Simple{  
+    public static void main(String args[]){  
+     for (int i = 0; i < 5; i++) {
+       System.out.println(i);
+     }
+       System.out.println("Hello Java");  
+       String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+      for (String i : cars) {
+         System.out.println(i);
+      }
+    } 
+}  
